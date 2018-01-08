e2b551267af04ef55d191eab408d87893dac1fb7
bezdvor <bezdvorniyalex@gmail.com>
2018-01-08 16:49:20 +0200

    add materialise Landing

---
+++ Materialise_LANDING/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Note_Bezdvor on 22.12.2017.
+ */
+$(document).ready(function() {
+    $(".dropdown-button").dropdown();
+    $('select').material_select();
+    $('.carousel').carousel();
+    $('.collapsible').collapsible();
+    $('.parallax').parallax();
+    $(".button-collapse").sideNav();
+});
